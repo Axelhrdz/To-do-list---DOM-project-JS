4bc75129c0703c8d8bbd01f838b40513371dc0ae
Axel <axel@Axel-Hernandez-L77X5JMQ7R.local>
2024-08-08 21:05:15 -0600

    add list items functionality done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Getting elements
 const form = document.getElementById('form');
 const input = document.getElementById('task');
+const mainContainer = document.querySelector('.main--container');
 
 
 console.log(form);
@@ -13,4 +14,25 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
 
     console.log(input.value)
+
+
+    const newTask = document.createElement("div.test");
+    newTask.classList.add("test");
+    console.log(newTask);
+
+    newTask.innerHTML = 
+    `<span>${input.value}</span>
+        <div class="test--icons">
+            <ion-icon name="trash-sharp" id="delete"></ion-icon>
+            <ion-icon name="checkmark-circle-sharp" id="check"></ion-icon>
+        </div>`
+
+     mainContainer.appendChild(newTask); 
+
+
+
+
+
+
+    input.value = '';
 })
